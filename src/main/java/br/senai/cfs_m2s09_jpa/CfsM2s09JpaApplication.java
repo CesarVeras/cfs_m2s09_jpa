34a34121657e5d2ca3ee3a20cb5859136dac24dd
Eduardo Cesar Veras dos Santos <eduardocvsantos@gmail.com>
2023-07-14 23:19:17 -0300

    commit inicial

---
+++ src/main/java/br/senai/cfs_m2s09_jpa/CfsM2s09JpaApplication.java
@@ -0,0 +1,13 @@
+package br.senai.cfs_m2s09_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CfsM2s09JpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CfsM2s09JpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/senai/cfs_m2s09_jpa/CfsM2s09JpaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.senai.cfs_m2s09_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CfsM2s09JpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
